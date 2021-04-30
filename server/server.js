d7f7bdb067c1c15e2a7ab0008bf7861e6fa2f01a
esroleo <esroleo@gmail.com>
2021-04-30 14:00:57 -0400

    server.js set up completed

--- server/server.js
+++ server/server.js
@@ -1,21 +1,68 @@
-const express = require('express');
+// import the path module
 const path = require('path');
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// middle function for authentication
+const { authMiddleware } = require('./utils/auth');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // conext will be used for the the token authentication
+  // these are HTTP headers
+  // HTTP middlware
+  // before adding middleware
+  //context: ({ req }) => req.headers
+  // after adding middleare
+  context: authMiddleware
+});
 
-app.use(express.urlencoded({ extended: true }));
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
+// Serve up static assets
+// Back end code to serve up React front-end code in production
+// 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// wildcard get route for the server. If we make a get request 
+// to any location on the server that doesn't have an explicit
+// route defined, respond with the production -ready react front-end codde
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
+
+// BEFORE REFACTOR CODE BELOW
+//const routes = require('./routes');
+
+
+//app.use(routes);
+
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
